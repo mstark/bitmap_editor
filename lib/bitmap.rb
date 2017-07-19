e6b3e3925f1dd6801aee240143968e840d7d62e8
Michael Stark <mstark@gmx.org>
2017-07-19 20:47:28 +0200

    add missing specs, fix bitmap rendering, fix warning

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,20 +1,20 @@
 class Bitmap
   attr_reader :dimensions, :colors
-  BASE_COLOR = "O"
+  Dimensions = Struct.new(:rows, :cols)
 
-  def initialize(cols:, rows:)
-    Struct.new("Dimensions", :rows, :cols)
-    @dimensions = Struct::Dimensions.new(rows, cols)
+  def initialize(cols:, rows:, base_color: "O")
+    @dimensions = Dimensions.new(rows, cols)
     @colors = {}
+    @base_color = base_color
   end
 
   def draw
     bitmap = ""
-    dimensions.rows.times do |x|
+    dimensions.rows.times do |y|
       row = ""
-      dimensions.cols.times do |y|
+      dimensions.cols.times do |x|
         pos = [x + 1, y + 1].join("-")
-        row += colors[pos] ? colors[pos] : BASE_COLOR
+        row += colors.has_key?(pos) ? colors[pos] : @base_color
       end
       bitmap += row + "\n"
     end
@@ -22,20 +22,22 @@ class Bitmap
   end
 
   def reset!
-    @colors = {}
+    colors.clear
   end
 
   def set_pixel_color(x:, y:, color:)
-    colors[[x, y].join("-")] = color
+    colors.store([x, y].join("-"), color)
   end
 
   def draw_vertical_line(x:, y1:, y2:, color:)
+    y1, y2 = y2, y1 if y2 < y1 # flip values in case y2 is less than y1
     (y1..y2).each do |y|
       set_pixel_color(x: x, y: y, color: color)
     end
   end
 
   def draw_horizontal_line(y:, x1:, x2:, color:)
+    x1, x2 = x2, x1 if x2 < x1 # flip values in case x2 is less than x1
     (x1..x2).each do |x|
       set_pixel_color(x: x, y: y, color: color)
     end
--- spec/lib/bitmap_spec.rb
+++ spec/lib/bitmap_spec.rb
@@ -6,4 +6,76 @@ RSpec.describe Bitmap do
       ).to eq("OOOOO\nOOOOO\nOOOOO\nOOOOO\nOOOOO\n")
     end
   end
+
+  describe "set_pixel_color" do
+    let(:bitmap) { Bitmap.new(cols: 1, rows: 2) }
+    before do
+      bitmap.set_pixel_color(x: 1, y: 2, color: "W")
+    end
+
+    it "changes given pixel to given color" do
+      expect(bitmap.draw).to eq("O\nW\n")
+    end
+  end
+
+  describe "draw_vertical_line" do
+    let(:bitmap) { Bitmap.new(cols: 1, rows: 5) }
+
+    context "y values in order" do
+      before do
+        bitmap.draw_vertical_line(x: 1, y1: 2, y2: 4, color: "B")
+      end
+
+      it "draws vertical line with given params" do
+        expect(bitmap.draw).to eq("O\nB\nB\nB\nO\n")
+      end
+    end
+
+    context "y values in reverse order" do
+      before do
+        bitmap.draw_vertical_line(x: 1, y1: 4, y2: 2, color: "B")
+      end
+
+      it "draws vertical line with given params" do
+        expect(bitmap.draw).to eq("O\nB\nB\nB\nO\n")
+      end
+    end
+  end
+
+  describe "draw_horizontal_line" do
+    let(:bitmap) { Bitmap.new(cols: 5, rows: 2) }
+
+    context "x values in order" do
+      before do
+        bitmap.draw_horizontal_line(y: 2, x1: 2, x2: 4, color: "B")
+      end
+
+      it "draws horizontal line with given params" do
+        expect(bitmap.draw).to eq("OOOOO\nOBBBO\n")
+      end
+    end
+
+    context "x values in reverse order" do
+      before do
+        bitmap.draw_horizontal_line(y: 2, x1: 4, x2: 2, color: "B")
+      end
+
+      it "draws horizontal line with given params" do
+        expect(bitmap.draw).to eq("OOOOO\nOBBBO\n")
+      end
+    end
+  end
+
+  describe "reset!" do
+    let(:bitmap) { Bitmap.new(cols: 5, rows: 1) }
+    before do
+      bitmap.set_pixel_color(x: 3, y: 1, color: "B")
+    end
+
+    it "resets bitmap to background color" do
+      expect(bitmap.draw).to eq("OOBOO\n")
+      bitmap.reset!
+      expect(bitmap.draw).to eq("OOOOO\n")
+    end
+  end
 end
