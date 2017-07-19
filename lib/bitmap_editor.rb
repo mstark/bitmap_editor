cf95567aff9a96e083f6aa17dc3ffdba22c835c6
Michael Stark <mstark@gmx.org>
2017-07-19 23:08:36 +0200

    add command parsing and bitmap handling to run function

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -4,16 +4,41 @@ require_relative "bitmap"
 class BitmapEditor
 
   def run(file)
-    return puts "please provide correct file" if file.nil? || !File.exists?(file)
-
+    return puts "please provide correct file" if file.nil? || !File.exist?(file)
+    @bitmap = nil
     File.open(file).each do |line|
       line = line.chomp
-      case line
-      when 'S'
-          puts "There is no image"
+      command = Command.new(line)
+
+      if command.create?
+        params = command.parameters
+        @bitmap = Bitmap.new(cols: params[:n], rows: params[:m])
+        next
+      elsif @bitmap
+        if command.show?
+          puts @bitmap.draw
+        elsif command.clear?
+          @bitmap.reset!
+        elsif command.colors?
+          params = command.parameters
+          @bitmap.set_pixel_color(x: params[:x], y: params[:y], color: params[:color])
+        elsif command.vertical?
+          params = command.parameters
+          @bitmap.draw_vertical_line(x: params[:x], y1: params[:y1], y2: params[:y2], color: params[:color])
+        elsif command.horizontal?
+          params = command.parameters
+          @bitmap.draw_horizontal_line(y: params[:y], x1: params[:x1], x2: params[:x2], color: params[:color])
+        end
       else
-          puts 'unrecognised command :('
+        puts 'unrecognised command :('
       end
+
+      # case line
+      # when 'S'
+      #     puts "There is no image"
+      # else
+      #     puts 'unrecognised command :('
+      # end
     end
   end
 end
--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -2,10 +2,20 @@ RSpec.describe BitmapEditor do
   include FileHelper
 
   Dir["spec/support/examples/input/*.txt"].each do |path|
-    it "#{path}" do
-      expect(
-        %x[bin/bitmap_editor #{path}]
-      ).to eq output_file(path)
+    describe "bin/bitmap_editor" do
+      it "#{path}" do
+        expect(
+          %x[bin/bitmap_editor #{path}]
+        ).to eq output_file(path)
+      end
+    end
+
+    describe "#run" do
+      it "#{path}" do
+        expect {
+          BitmapEditor.new.run(path)
+        }.to output(output_file(path)).to_stdout
+      end
     end
   end
 end
