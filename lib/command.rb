dc39ca04a00e568cf62a316380692e3c727e9573
Michael Stark <mstark@gmx.org>
2017-07-19 21:43:44 +0200

    add logic for vertical and horizontal method

--- lib/command.rb
+++ lib/command.rb
@@ -6,7 +6,7 @@ class Command
   def initialize(command)
     @matches = nil
     @command = command
-    @params = nil
+    @result = nil
   end
 
   # I M N
@@ -49,10 +49,38 @@ class Command
 
   # V X Y1 Y2 C
   def vertical?
+    m = /\A(V)\s{1}(\d{1,3})\s{1}(\d{1,3})\s{1}(\d{1,3})\s{1}([A-Z]{1})\z/.match(command)
+
+    x = m && m[2] ? m[2].to_i : 0
+    y1 = m && m[3] ? m[3].to_i : 0
+    y2 = m && m[4] ? m[4].to_i : 0
+    color = m && m[5] ? m[5] : ""
+
+    if RANGE.include?(x) && RANGE.include?(y1) && RANGE.include?(y2) && COLOR.include?(color)
+      @result = { command: :vertical, x: x, y1: y1, y2: y2, color: color }
+      true
+    else
+      @result = { errors: "" }
+      false
+    end
   end
 
   # H X1 X2 Y C
   def horizontal?
+    m = /\A(H)\s{1}(\d{1,3})\s{1}(\d{1,3})\s{1}(\d{1,3})\s{1}([A-Z]{1})\z/.match(command)
+
+    x1 = m && m[2] ? m[2].to_i : 0
+    x2 = m && m[3] ? m[3].to_i : 0
+    y = m && m[4] ? m[4].to_i : 0
+    color = m && m[5] ? m[5] : ""
+
+    if RANGE.include?(y) && RANGE.include?(x1) && RANGE.include?(x2) && COLOR.include?(color)
+      @result = { command: :horizontal, x1: x1, x2: x2, y: y, color: color }
+      true
+    else
+      @result = { errors: "" }
+      false
+    end
   end
 
   def show?
@@ -60,5 +88,6 @@ class Command
   end
 
   def parameters
+    @result
   end
 end
